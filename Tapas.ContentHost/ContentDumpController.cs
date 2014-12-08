5067b9a0871f48fb67b6f89b7aab2a6d869041c9
joeriks <jonas@programenta.se>
2014-12-08 14:06:59 +0100

    fixed the host controller

--- Tapas.ContentHost/ContentDumpController.cs
+++ Tapas.ContentHost/ContentDumpController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
 
 namespace Tapas
 {
