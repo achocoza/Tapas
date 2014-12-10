c78c2b209ed051ac90095a1e576b69e9ce1a895c
joeriks <jonas@programenta.se>
2014-12-11 00:13:07 +0100

    stub for publish info

---
+++ Tapas.ContentHost/OnPublishEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using umbraco.BusinessLogic;
+using umbraco.cms.businesslogic;
+using umbraco.cms.businesslogic.web;
+using Umbraco.Core;
+
+namespace Tapas
+{
+    public class RegisterEvents : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {            
+            Document.BeforePublish += Document_BeforePublish;
+            Document.BeforeUnPublish += Document_BeforeUnPublish;
+            Document.BeforeDelete += Document_BeforeDelete;
+        }
+
+        void Document_BeforeDelete(Document sender, DeleteEventArgs e)
+        {
+            // send to tapas client
+        }
+
+        void Document_BeforeUnPublish(Document sender, UnPublishEventArgs e)
+        {
+            // send to tapas client
+        }
+
+        private void Document_BeforePublish(Document sender, PublishEventArgs e)
+        {
+            // send to tapas client
+        }
+    }
+}
