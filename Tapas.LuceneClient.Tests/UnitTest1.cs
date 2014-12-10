0dc20b64196afa0011cad51c71f31ee7c8f4d2b4
joeriks <jonas@programenta.se>
2014-12-10 23:45:52 +0100

    successfully added nrtsearch

--- Tapas.LuceneClient.Tests/UnitTest1.cs
+++ Tapas.LuceneClient.Tests/UnitTest1.cs
@@ -10,66 +10,97 @@ namespace Tapas.LuceneClient.Tests
         [TestMethod]
         public void CreateIndexTest()
         {
-
-            PortableNodeIndex.Path = @"C:\admin\indexThree";
-
-            PortableNodeIndex.AddNode(new PortableNode
+            ContentService.Initialize(@"C:\admin\indexThree");
+            ContentService.DeleteAll();
+            ContentService.AddNode(new PortableNode
             {
                 Url = "/",
                 Name = "Home",
                 Id = 1
             });
-            PortableNodeIndex.AddNode(new PortableNode
+            ContentService.AddNode(new PortableNode
             {
                 Url = "/Foo",
                 Name = "Foo",
                 Id = 2,
                 ParentId = 1
             });
-            PortableNodeIndex.AddNode(new PortableNode
+            ContentService.AddNode(new PortableNode
             {
                 Url = "X",
                 Name = "x",
                 Id = 3, ParentId =1
             });
+            ContentService.Dispose();
 
         }
 
         [TestMethod]
+        public void FindTests()
+        {
+            ContentService.Initialize(@"C:\admin\indexThree");
+            var foo1 = ContentService.GetById(2);
+            Assert.AreEqual(2, foo1.Id);
+
+            var foo2 = ContentService.GetById(3);
+            Assert.AreEqual(3, foo2.Id);
+
+            var fooName = ContentService.GetByName("Foo");
+            Assert.AreEqual(fooName.Id, 2);
+
+            var foo3 = ContentService.GetByUrl("X");
+            Assert.AreEqual(3, foo3.Id);
+
+            var foo4 = ContentService.GetByUrl(@"/Foo");
+            Assert.AreEqual("/Foo", foo4.Url);
+
+            var home = ContentService.GetByUrl(@"/");
+
+
+            Assert.AreEqual("Home", home.Name);
+            Assert.AreEqual("Foo", foo1.Name);
+
+            ContentService.Dispose();
+        }
+
+
+        [TestMethod]
         public void Delete()
         {
-            PortableNodeIndex.Path = @"C:\admin\indexThree"; 
-            PortableNodeIndex.DeleteAll();
+            ContentService.Initialize(@"C:\admin\indexThree");
+            ContentService.DeleteAll();
         }
 
         [TestMethod]
         public void IndexChange()
         {
-            PortableNodeIndex.Path = @"C:\admin\indexThree";
-            PortableNodeIndex.DeleteAll();
-            
+
             ContentService.Initialize(@"C:\admin\indexThree");
-            PortableNodeIndex.AddNode(new PortableNode
+            ContentService.DeleteAll();
+
+            ContentService.AddNode(new PortableNode
             {
                 Url = "X",
                 Name = "x",
                 Id = 3,
                 ParentId = 1
-            });
-            ContentService.ReInitialize();
+            }, true);
+
             var foo1 = ContentService.GetById(3);
 
             Assert.AreEqual("x", foo1.Name);
-            PortableNodeIndex.AddNode(new PortableNode
+            ContentService.AddNode(new PortableNode
             {
                 Url = "X",
                 Name = "Changed",
                 Id = 3,
                 ParentId = 1
-            });
-            ContentService.ReInitialize(); 
+            }, true);
+
             var foo2 = ContentService.GetById(3);
             Assert.AreEqual("Changed", foo2.Name);
+            
+            ContentService.Dispose();
         }
 
         [TestMethod]
@@ -86,31 +117,6 @@ namespace Tapas.LuceneClient.Tests
 
             Assert.AreEqual(2, children.Count());
 
-        }
-        [TestMethod]
-        public void FindTests()
-        {
-            ContentService.Initialize(@"C:\admin\indexThree");
-            var foo1 = ContentService.GetById(2);
-            Assert.AreEqual(2, foo1.Id);
-
-            var foo2 = ContentService.GetById(3);
-            Assert.AreEqual(3, foo2.Id);
-
-            var fooName = ContentService.GetByName("foo");
-            Assert.AreEqual(fooName.Id, 2);
-
-            var foo3 = ContentService.GetByUrl("X");
-            Assert.AreEqual(3, foo3.Id);
-
-            var foo4 = ContentService.GetByUrl(@"/Foo");
-            Assert.AreEqual("/Foo", foo4.Url);
-
-            var home = ContentService.GetByUrl(@"/");
-
-
-            Assert.AreEqual("Home", home.Name);
-            Assert.AreEqual("Foo", foo1.Name);
         }
     }
 }
--- Tapas.LuceneClient/ContentService.cs
+++ Tapas.LuceneClient/ContentService.cs
@@ -1,6 +1,9 @@
-﻿using Lucene.Net.Documents;
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Contrib.Management.Client;
+using Lucene.Net.Documents;
 using Lucene.Net.Index;
 using Lucene.Net.Search;
+using Lucene.Net.Store;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,60 +14,57 @@ namespace Tapas.LuceneClient
 {
     public class ContentService
     {
-        public static IndexSearcher Searcher { get; set; }
+        public static SearcherContext Searcher { get; private set; }
+
+        //public static IndexSearcher Searcher { get; set; }
         private static string indexPath;
         public static void Initialize(string path)
         {
             indexPath = path;
-            //state the file location of the index
-            var indexFileLocation = new System.IO.DirectoryInfo(path);
-            Lucene.Net.Store.Directory dir = Lucene.Net.Store.FSDirectory.Open(indexFileLocation);
-            //create an index searcher that will perform the search
-            Searcher = new
-            Lucene.Net.Search.IndexSearcher(dir);
+            
+            Searcher = new SearcherContext(FSDirectory.Open(path), new WhitespaceAnalyzer());
             
         }
-        public static void ReInitialize()
-        {
-            Searcher.Dispose();
-            var indexFileLocation = new System.IO.DirectoryInfo(indexPath);
-            Lucene.Net.Store.Directory dir = Lucene.Net.Store.FSDirectory.Open(indexFileLocation);
-            //create an index searcher that will perform the search
-            Searcher = new
-            Lucene.Net.Search.IndexSearcher(dir);
 
-        }
         public static PortableNode GetBy(string fieldName, string value)
         {
-            //build a query object
-            Lucene.Net.Index.Term searchTerm = new Lucene.Net.Index.Term(fieldName, value);
-            Lucene.Net.Search.Query query = new Lucene.Net.Search.TermQuery(searchTerm);
+            using (var search = Searcher.GetSearcher())
+            {
 
-            TopScoreDocCollector collector = TopScoreDocCollector.Create(1, true);
+                //build a query object
+                Lucene.Net.Index.Term searchTerm = new Lucene.Net.Index.Term(fieldName, value);
+                Lucene.Net.Search.Query query = new Lucene.Net.Search.TermQuery(searchTerm);
 
-            //execute the query
-            Searcher.Search(query, collector);
-            ScoreDoc[] hits = collector.TopDocs().ScoreDocs;
+                TopScoreDocCollector collector = TopScoreDocCollector.Create(1, true);
 
-            if (hits.Length == 1) return Converters.ConvertToPortableNode(Searcher.IndexReader.Document(hits[0].Doc));
+                //execute the query
+                search.Searcher.Search(query, collector);
+                ScoreDoc[] hits = collector.TopDocs().ScoreDocs;
 
-            return null;
+                if (hits.Length == 1) return Converters.ConvertToPortableNode(search.Searcher.Doc(hits[0].Doc));
+
+                return null;
+            }
         }
         public static List<PortableNode> GetAllBy(string fieldName, string value)
         {
-            //build a query object
-            Lucene.Net.Index.Term searchTerm = new Lucene.Net.Index.Term(fieldName, value);
-            Lucene.Net.Search.Query query = new Lucene.Net.Search.TermQuery(searchTerm);
+            using (var search = Searcher.GetSearcher())
+            {
 
-            TopScoreDocCollector collector = TopScoreDocCollector.Create(1000, true);
+                //build a query object
+                Lucene.Net.Index.Term searchTerm = new Lucene.Net.Index.Term(fieldName, value);
+                Lucene.Net.Search.Query query = new Lucene.Net.Search.TermQuery(searchTerm);
 
-            //execute the query
-            Searcher.Search(query, collector);
-            ScoreDoc[] hits = collector.TopDocs().ScoreDocs;
-            if (hits != null)
-                return hits.Select(t => Searcher.IndexReader.Document(t.Doc)).Select(t => Converters.ConvertToPortableNode(t)).ToList();
+                TopScoreDocCollector collector = TopScoreDocCollector.Create(1000, true);
 
-            return null;
+                //execute the query
+                search.Searcher.Search(query, collector);
+                ScoreDoc[] hits = collector.TopDocs().ScoreDocs;
+                if (hits != null)
+                    return hits.Select(t => search.Searcher.Doc(t.Doc)).Select(t => Converters.ConvertToPortableNode(t)).ToList();
+
+                return null;
+            }
         }
         public static PortableNode GetByUrl(string url)
         {
@@ -72,7 +72,7 @@ namespace Tapas.LuceneClient
         }
         public static PortableNode GetByName(string name)
         {
-            return GetBy("Name", name.ToLower());
+            return GetBy("Name", name);
         }
         public static PortableNode GetById(int Id)
         {
@@ -81,6 +81,38 @@ namespace Tapas.LuceneClient
         public static List<PortableNode> GetByParentId(int Id)
         {
             return GetAllBy("ParentId", Id.ToString());
+        }
+        public static void DeleteAll()
+        {
+            ContentService.Searcher.Manager.DeleteAll();
+        }
+        public static void AddNode(PortableNode portableNode, bool wait = false)
+        {
+
+            var doc = Converters.ConvertToDocument(portableNode);
+            //write the document to the index
+
+            Lucene.Net.Index.Term searchTerm = new Lucene.Net.Index.Term("Id", portableNode.Id.ToString());
+            Lucene.Net.Search.Query query = new Lucene.Net.Search.TermQuery(searchTerm);
+            
+            ContentService.Searcher.Manager.DeleteDocuments(query);
+
+            var gen = ContentService.Searcher.Manager.AddDocument(doc);
+
+            if (wait) ContentService.Searcher.Manager.WaitForGeneration(gen);
+
+        }
+        public static void DeleteNode(int Id)
+        {
+            Lucene.Net.Index.Term searchTerm = new Lucene.Net.Index.Term("Id", Id.ToString());
+            Lucene.Net.Search.Query query = new Lucene.Net.Search.TermQuery(searchTerm);
+            ContentService.Searcher.Manager.DeleteDocuments(query);
+        }
+
+
+        public static void Dispose()
+        {
+            Searcher.Dispose();
         }
     }
 }
