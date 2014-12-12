60cf0c16064b6aad3da3679d7808b833f4f1b152
joeriks <jonas@programenta.se>
2014-12-12 09:57:10 +0100

    Core tests

--- Tapas.Core.Tests/Properties/AssemblyInfo.cs
+++ Tapas.Core.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tapas.Models")]
+[assembly: AssemblyTitle("Tapas.Core.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tapas.Models")]
+[assembly: AssemblyProduct("Tapas.Core.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9c25a448-6832-4c6e-8a35-7420c9d1bb05")]
+[assembly: Guid("5d1c6d8c-cb16-4cfe-835d-8e6651bca135")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tapas.Core.Tests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Tapas.Core.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SpinUpAInmemory()
+        {
+            var s = new PortableNodesInMemory();
+            s.Add(new PortableNode { Id = 1, Name = "Foo" });
+            s.Add(new PortableNode { Id = 2, Name = "Bar", ParentId=1 });
+
+            Assert.AreEqual(1, s.GetById(1).Id);
+            Assert.AreEqual(2, s.GetById(2).Id);
+            Assert.AreEqual(1, s.GetById(2).ParentId);
+            
+        }
+
+        [TestMethod]
+        public void Walk()
+        {
+            var s = new PortableNodesInMemory();
+            s.Add(new PortableNode { Id = 1, Name = "Foo" });
+            s.Add(new PortableNode { Id = 2, Name = "Bar", ParentId = 1 });
+
+            Assert.AreEqual(1, s.GetById(1).Id);
+            Assert.AreEqual(1, s.Parent(s.GetById(2)).Id);
+            Assert.AreEqual(2, s.Children(s.GetById(1)).FirstOrDefault().Id);
+
+        }
+    }
+}
